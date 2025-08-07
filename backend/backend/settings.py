84b3066dbb4111175ee9c33883b0c145209e6e47
Sanal Sabu <sanalsabu22@gmail.com>
2025-08-07 17:18:41 +0530

    feat(security): applied CORS security patch, enabled bcrypt password hashing, and created superuser

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -118,6 +118,18 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+
+
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",   
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.ScryptPasswordHasher",
+]
+
+
+
 # User model
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
